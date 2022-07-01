aa61d568107fe2cc362dfb9a82820c12fb8f82d3
Silvan Flum <flumsil1@students.zhaw.ch>
2022-07-01 13:13:13 +0200

    Revert

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <h2>This Is A Work In Progress</h2>
+    </div>
+  );
+}
+
+export default App;
